78422b7ef003c8dd9e246408448c5bff63d14153
Lucas Anacleto <lucasanacleto@outlook.com.br>
2021-11-03 17:39:41 -0300

    adding one more character detail

--- src/components/CharCard/index.jsx
+++ src/components/CharCard/index.jsx
@@ -1,12 +1,16 @@
 import "./style.css";
 
 const CharCard = ({ person }) => {
+  let personName = person.name;
+
+  if (person.name.length > 15) {
+    personName = person.name.substr(0, 15) + "...";
+  }
   return (
     <div className={person.status}>
-      <p>{person.name}</p>
-      {console.log(person)}
-      {console.log(person.status)}
+      <p>{personName}</p>
       <img src={person.image} alt="" />
+      <p className="specie-name">{person.species}</p>
     </div>
   );
 };
